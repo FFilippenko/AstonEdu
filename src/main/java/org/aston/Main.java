7c48072d2a9c8c593b068c19c605afb087da5c0c
FFilippenko <fifil@yandex.ru>
2024-12-24 00:32:47 +0300

    Lesson 3 (1-7)

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -4,4 +4,62 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    // Задание 1
+    public static void printThreeWords() {
+        System.out.println("Orange,\nBanana,\nApple");
+    }
+
+    // Задание 2
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -6;
+        if (a + b > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    // Задание 3
+    public static void printColor() {
+        int value = 1000;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // Задание 4
+    static void compareNumbers() {
+        int a = 100;
+        int b = -1;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+
+    // Задание 5
+    static boolean checkSum(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    // Задание 6
+    static void printNumberSign(int a) {
+        if (a < 0) {
+            System.out.println("Отрицательное число");
+        } else {
+            System.out.println("Положительное число");
+        }
+    }
+
+    // Задание 7
+    static boolean checkNumberSign(int a) {
+        return a < 0;
+    }
 }
